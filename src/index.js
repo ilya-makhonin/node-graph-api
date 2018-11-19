e090efede99fc4ef3bc5b61fc567596750c25f8a
Ilia Makhonin <noizemkutane77@gmail.com>
2018-11-20 02:05:21 +0300

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const hapi = require('hapi');
 const mongoose = require('mongoose');
+const Painting = require('./models/painting');
 
 const server = hapi.server({
     port: 4000,
@@ -8,23 +9,44 @@ const server = hapi.server({
 
 
 const init = async () => {
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler(request, reply) {
-            return `<h1>Our new API</h1>`;
+    server.route([
+        {
+            method: 'GET',
+            path: '/',
+            handler(request, reply) {
+                return `<h1>Our new API</h1>`;
+            }
+        },
+        {
+            method: 'GET',
+            path: '/api/v1/paintings',
+            handler(req, reply) {
+                return Painting.find();
+            }
+        },
+        {
+            method: 'POST',
+            path: '/api/v1/paintings',
+            handler: (req, reply) => {
+                const { name, url, technique } = req.payload;
+                const painting = new Painting({
+                    name, url, technique
+                });
+                return painting.save();
+            }
         }
-    });
+    ]);
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
 };
 
 mongoose.connect(
-    'mongodb://admin:naizemk123@ds239137.mlab.com:39137/node-power-api',
+    'mongodb://admin:123456zxc@ds239137.mlab.com:39137/node-power-api',
     { useNewUrlParser: true }
 );
+
 mongoose.connection.once('open', () => {
-    console.log('Connected to database')
+    console.log('Connected to database!');
 });
 
 init();
