1a57346b21ea162aeddfbe900ffb441330c12278
Ilia Makhonin <noizemkutane77@gmail.com>
2018-11-20 14:42:01 +0300

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,60 +1,88 @@
 const hapi = require('hapi');
 const mongoose = require('mongoose');
 const Painting = require('./models/painting');
+const { graphqlHapi, graphiqlHapi } = require('apollo-server-hapi');
+const schema = require('./graphql/schema');
 
 const server = hapi.server({
     port: 4000,
     host: 'localhost'
 });
 
+server.route([
+    {
+        method: 'GET',
+        path: '/',
+        handler(request, reply) {
+            return `<h1>Our new API</h1>`;
+        }
+    },
+    {
+        method: 'GET',
+        path: '/api/v1/paintings',
+        handler(req, reply) {
+            return Painting.find();
+        }
+    },
+    {
+        method: 'POST',
+        path: '/api/v1/paintings',
+        handler: (req, reply) => {
+            const { name, url, technique } = req.payload;
+            const painting = new Painting({
+                name, url, technique
+            });
+            return painting.save();
+        }
+    },
+    {
+        method: 'POST',
+        path: '/api/v1/paintings/del',
+        handler: (req, reply) => {
+            const { name } = req.payload;
+            if (name === '') {
+                Painting.deleteMany({}, (err) => {
+                    if (err) return err;
+                });
+                console.log('It\'s many deleting');
+            } else {
+                Painting.deleteOne({ name: name }, (err) => {
+                    if (err) return err;
+                });
+                console.log('It\'s one deleting');
+            }
+            return Painting.find();
+        }
+    },
+]);
 
 const init = async () => {
-    server.route([
-        {
-            method: 'GET',
-            path: '/',
-            handler(request, reply) {
-                return `<h1>Our new API</h1>`;
+    await server.register({
+        plugin: graphiqlHapi,
+        options: {
+            path: '/graphiql',
+            graphiqlOptions: {
+                endpointURL: '/graphql'
+            },
+            route: {
+                cors: true
             }
-        },
-        {
-            method: 'GET',
-            path: '/api/v1/paintings',
-            handler(req, reply) {
-                return Painting.find();
+        }
+    });
+
+    await server.register({
+        plugin: graphqlHapi,
+        options: {
+            path: '/graphql',
+            graphqlOptions: {
+                schema
+            },
+            route: {
+                cors: true
             }
-        },
-        {
-            method: 'POST',
-            path: '/api/v1/paintings',
-            handler: (req, reply) => {
-                const { name, url, technique } = req.payload;
-                const painting = new Painting({
-                    name, url, technique
-                });
-                return painting.save();
-            }
-        },
-        {
-            method: 'POST',
-            path: '/api/v1/paintings/del',
-            handler: (req, reply) => {
-                const { name } = req.payload;
-                if (name === '') {
-                    Painting.deleteMany({}, (err) => {
-                        if (err) return err;
-                    });
-                    console.log('It\'s many deleting');
-                } else {
-                    Painting.deleteOne({ name: name }, (err) => {
-                        if (err) return err;
-                    });
-                    console.log('It\'s one deleting');
-                }
-                return Painting.find();
-            }
-        },
-    ]);
+        }
+    });
+
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
 };
